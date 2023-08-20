41ff362d9204eb805e6666024699b7eb0f1abf98
ponmathi <ponmathittl@gmail.com>
2023-08-20 09:31:02 +0530

    ExpenseDetails

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-// import logo from './logo.svg';
-// import './App.css';
-import ExpenseItem from './Compontents/ExpenseItem';
+import ExpenseItem from "./Compontents/ExpenseItem";
 
 function App() {
-    return (
-        <div>
-            <ExpenseItem></ExpenseItem>
-        </div>
-    );
+  const expenses = [
+    {
+      id: "e1",
+      title: "Tissue",
+      amount: 150,
+    },
+    {
+       id: "e2", 
+       title: "Laptop",
+        amount: 15999.00,
+    },
+    {
+      id: "e3",
+      title: "Mobile",
+      amount: 59999.50,
+    },
+    {
+      id: "e4",
+      title: "Earpods",
+      amount: 8000.00,
+    },
+  ];
+  return (
+    <div>
+        <ExpenseItem
+            title={expenses[0].title}
+            amount={ expenses[0].amount}
+            date={ expenses[0].date}>
+        </ExpenseItem>
+        <ExpenseItem  
+            title={expenses[1].title}
+            amount={expenses[1].amount}
+            date={ expenses[1].date}>
+        </ExpenseItem>
+        <ExpenseItem  
+            title={expenses[2].title}
+            amount={expenses[2].amount}
+            date={ expenses[2].date}>
+        </ExpenseItem>
+        <ExpenseItem  
+            title={expenses[3].title}
+            amount={expenses[3].amount}
+            date={ expenses[3].date}>
+        </ExpenseItem>
+    </div>
+  );
 }
 export default App;
---
+++ src/components/ExpenseDetails.js
@@ -0,0 +1,18 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseDetails(props){
+    const month = props.date.toLocaleString('en-Us', { month: 'long'});
+    const day = props.date.toLocaleString('en-Us', { day: '2-digit'});
+    const year = props.date.getFullYear();
+    return(
+        <div className="expense-item">
+            <ExpenseDate date= {props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>
+    )
+}
+export default ExpenseDetails;
