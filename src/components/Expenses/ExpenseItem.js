7fbf1dd04cc37d9d1319b599c3a6cc332ea7af34
ponmathi <ponmathittl@gmail.com>
2023-08-25 07:40:21 +0530

    single expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,10 +6,12 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
     return (
-      <Card className='expense-item'>
-        <ExpenseDate date = {props.date} />
-        <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-      </Card>
+      <li>
+        <Card className='expense-item'>
+          <ExpenseDate date = {props.date} />
+          <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+        </Card>
+      </li>
     ); 
 }
 export default ExpenseItem;
